f03f77353cc5a6cf42b29f942fbd0af0fa32d735
Lukas <lukasimbm@gmail.com>
2021-06-27 18:49:10 +0300

    video 4

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    foreach (range(1, 100) as $i) {
-        \App\Jobs\SendWelcomeEmail::dispatch();
-    }
+    $chain = [
+        new PullRepo('laracasts/project1'),
+        new PullRepo('laracasts/project2'),
+        new PullRepo('laracasts/project3')
+    ];
+
+//    \Illuminate\Support\Facades\Bus::chain($chain)->dispatch(); // works like chain of responsibility pattern
+    \Illuminate\Support\Facades\Bus::batch($chain)->allowFailures()->dispatch(); // multiple workers will run in parallel
 
 //    \App\Jobs\SendWelcomeEmail::dispatch();
 
